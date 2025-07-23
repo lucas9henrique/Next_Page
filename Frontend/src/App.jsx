1e5200ea2511f3284963a54b3b5e36d7d10e9881
Lucas Henrique Gonzaga de Sales <71048167+lucas9henrique@users.noreply.github.com>
2025-07-23 00:16:37 -0300

    Add Projects page route and update login redirect
    Introduz uma nova rota para a página Projetos em App.jsx e atualiza o fluxo de login para redirecionar os usuários para /projects após a autenticação bem-sucedida em vez de /editor.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,12 +2,14 @@ import Login from './Pages/Login.jsx'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import Register from './Pages/Register.jsx'
 import Editor from './Pages/Editor.jsx'
+import Projects from './Pages/Projects.jsx'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/projects" element={<Projects />} />
       <Route path="/editor/:id" element={<Editor />} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
